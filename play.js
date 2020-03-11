b97a1ba1b824cf049e0760533b6f9ecd7c725289
yasu71 <yasuyo71@gmail.com>
2020-03-11 23:10:32 +0000

    setting up stdin

--- play.js
+++ play.js
@@ -2,3 +2,21 @@ const connect = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+// let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+};
+
+const handleUserInput = function(key) {
+  if (key === '\\q\n') {
+    client.end();
+    process.exit();
+  }
+};
